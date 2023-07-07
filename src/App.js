b56b66acfc1ddc46c627efad5163fcac051d34bf
johncovenant <ufotjohnamos@gmail.com>
2023-07-07 01:41:12 +0100

    error once again but we will stop here for tonight

--- src/App.js
+++ src/App.js
@@ -16,38 +16,12 @@ function App() {
     setTasks(loadedTasks);
   };
 
-  useHttp({ url: 'https://task-record-app-default-rtdb.firebaseio.com/tasks.json' });
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({ url: 'https://task-record-app-default-rtdb.firebaseio.com/tasks.json' }, transformTasks);
 
-  const fetchTasks = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://task-record-app-default-rtdb.firebaseio.com/tasks.json'
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+  // useEffect(() => {
+  //   fetchTasks();
+  // }, []);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
