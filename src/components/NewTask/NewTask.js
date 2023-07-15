dbad6480e12feed09b45df6eabb04920a793b63d
johncovenant <ufotjohnamos@gmail.com>
2023-07-15 01:13:10 +0100

    working on new task. js

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -22,36 +22,6 @@ const NewTask = (props) => {
       },
       body: { text: taskText }
     }, createTask);
-
-
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://task-record-app-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
   };
 
   return (
